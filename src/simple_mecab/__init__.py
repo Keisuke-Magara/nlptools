1e38d446c841135dc620e0ac9358d3658b3d0938
Keisuke-Magara <61485115+Keisuke-Magara@users.noreply.github.com>
2023-01-12 23:49:23 +0900

    Update __init__.py

--- src/simple_mecab/__init__.py
+++ src/simple_mecab/__init__.py
@@ -1,6 +1,7 @@
-from simple_mecab._version import version
+import simple_mecab._version
 from simple_mecab.exceptions import *
 from simple_mecab.mecab_wrapper import *
 from simple_mecab.morpheme import *
 
-__version__ = version
+__version__ = simple_mecab._version.version
+__version_tuple__ = simple_mecab._version.version_tuple
